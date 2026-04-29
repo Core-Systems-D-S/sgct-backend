ba59e65209849d4f93dda6db7ce300037ab61768
D13G0R <rdiegorodriguez48@gmail.com>
2026-04-29 07:48:38 -0500

    SGTC-HU-001 - Crud-completo-group
    Se finalizó el crud completo de groups

--- apps/groups/urls.py
+++ apps/groups/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import GroupAPIView
 urlpatterns = [
-    path('createGroup/', GroupAPIView.as_view(), name = "create_group")
+    path('', GroupAPIView.as_view(), name = "group"),
+    path('<int:pk>/', GroupAPIView.as_view(), name = "get_one_group")
 ]
 
--- apps/groups/views.py
+++ apps/groups/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
-from rest_framework. views import APIView
-from .serializer import GroupSerializer
+#importaciones de la biblioteca estandar
+
+#Importaciones de terceros
+from django.shortcuts import render, get_object_or_404
 from django.db import IntegrityError, transaction
+from rest_framework. views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import UserGroup
-# Create your views here.
+#Importaciones de mi app
+from .models import Group, UserGroup
+
+from .serializer import GroupSerializer
+
 class GroupAPIView(APIView):
 
     permission_classes = [IsAuthenticated]
@@ -17,7 +22,7 @@ class GroupAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         try:
             
-            with transaction.atomic(): 
+            with transaction.atomic():
 
                 group = serializer.save(owner= request.user)
 
@@ -32,4 +37,33 @@ class GroupAPIView(APIView):
         except IntegrityError:
 
             return Response("Hubo un problema con la base de datos.", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def get(self, request, pk=None):
+        if pk:
+            data = get_object_or_404(Group, id = pk)
+            serializer = GroupSerializer(data)
+        else:
+            data = Group.objects.all()
+            serializer = GroupSerializer(data, many = True)
+
+        return Response(data= serializer.data, status = status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+
+        existed = get_object_or_404(Group, id=pk)
+
+        serializer = GroupSerializer(existed, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=f"Se ha actualizado el registro {serializer.data.get('name')}", status=status.HTTP_200_OK)
+        
+        return  Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        group = get_object_or_404(Group, id = pk)
+
+        group.delete()
+
+        return Response(f"Se eliminó con exito el grupo con id {pk}", status = status.HTTP_200_OK)
         
