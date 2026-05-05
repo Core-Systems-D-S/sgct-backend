1042b32bdefd7bdd09be5e2c4820f127775cb3c0
Sebastian Grisales <grisales1214@gmail.com>
2026-05-04 22:01:08 -0500

    fix: added little things for backend and frontend connection

--- sgtc/settings.py
+++ sgtc/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'apps.users',
     'apps.tasks',
     'apps.groups',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +141,8 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME" : timedelta(days=1),
     "AUTH_HEADER_TIME" : ("Bearer",)
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite dev server
+    "http://localhost:3000",
+]
